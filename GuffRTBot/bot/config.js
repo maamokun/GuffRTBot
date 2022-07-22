cb4bcbefe43763f44cf6223cb792d6ec7a75af46
maamokun <45969559+maamokun@users.noreply.github.com>
2022-07-22 20:03:09 +0900

    再アップロードの準備

--- GuffRTBot/bot/config.js
+++
@@ -1,8 +0,0 @@
-// TwitterAPIキーをここに置く。READMEで詳細が知れます。
-module.exports = {
-    consumer_key:         'APIコンシュマーキー',
-    consumer_secret:      'APIコンシュマーシークレット',
-    access_token:         'アクセストークン',
-    access_token_secret:  'アクセスシークレット'
-  }
-  
